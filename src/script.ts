25f44fbd536c70df98c731da24a5cbe697d692ed
Mohammed Alshami <alshami@juleb.com>
2023-06-01 17:27:10 +0300

    pass

--- src/script.ts
+++ src/script.ts
@@ -109,9 +109,9 @@ class Script {
       (await promises.readFile("./params.json")).toString()
     );
     const sqlConfig = {
-      user: process.env.DB_USER || "finc3", //CHANGE THIS juleb-integration
-      password: process.env.DB_PWD || "AB.123456", //CHANGE THIS - 9!cf9voK
-      database: process.env.DB_NAME || "Integration", //CHANGE THIS - Integration_test
+      user: "juleb-integration", //CHANGE THIS juleb-integration
+      password: "9!cf9voK", //CHANGE THIS - 9!cf9voK
+      database: "Integration_test", //CHANGE THIS - Integration_test
       server: "192.168.1.9\\MSSQL2016", //LAPTOP-T8JG9PTD '192.168.1.8'
       pool: {
         max: 10,
